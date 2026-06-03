215e6adf2c0f5e58c8fcf666fafceb687e496675
Robert Sparks <robert.william.sparks@gmail.com>
2026-06-03 18:00:50 +0100

    replace Makefile with nix so we don't have hardcoded header files with links to fonts in nix store...

--- flake.nix
+++ flake.nix
@@ -4,38 +4,72 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }: let
-    system = "aarch64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
+    systems = [ "x86_64-linux" "aarch64-linux" ];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
   in {
-    devShells.${system}.default = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        raylib
-        dejavu_fonts
-        pkg-config
-      ];
-      shellHook = ''
-        cat > font_path.h <<EOF
-#define FONT_PATH "${pkgs.dejavu_fonts}/share/fonts/truetype/DejaVuSansMono.ttf"
-EOF
-      '';
-    };
+    packages = forAllSystems (system: let
+      pkgs = import nixpkgs { inherit system; };
+      fontPath = "${pkgs.dejavu_fonts}/share/fonts/truetype/DejaVuSansMono.ttf";
+    in {
+      default = pkgs.stdenv.mkDerivation {
+        pname = "cropper";
+        version = "0.1.0";
+        src = nixpkgs.lib.cleanSource ./.;
 
-    packages.${system}.default = pkgs.stdenv.mkDerivation {
-      pname = "cropper";
-      version = "0.1.0";
-      src = ./.;
-      buildInputs = with pkgs; [ raylib dejavu_fonts ];
-      nativeBuildInputs = with pkgs; [ pkg-config ];
-      buildPhase = ''
-        cat > font_path.h <<EOF
-#define FONT_PATH "${pkgs.dejavu_fonts}/share/fonts/truetype/DejaVuSansMono.ttf"
+        nativeBuildInputs = with pkgs; [ pkg-config ];
+        buildInputs = with pkgs; [ raylib dejavu_fonts ];
+
+        env.NIX_CFLAGS_COMPILE = "-std=c23 -Wall -Wextra -pedantic -O2";
+
+        preBuild = ''
+          cat > font_path.h <<EOF
+#define FONT_PATH "${fontPath}"
 EOF
-        make
+        '';
+
+        buildPhase = ''
+          $CC \
+            $(pkg-config --cflags raylib) \
+            $(pkg-config --libs raylib) -lm \
+            -include font_path.h \
+            -o cropper src/cropper.c
+        '';
+
+        installPhase = ''
+          mkdir -p $out/bin
+          cp cropper $out/bin/
+        '';
+
+        meta = with nixpkgs.lib; {
+          description = "Crop images to a given aspect ratio";
+          license = licenses.mit;
+          platforms = platforms.linux;
+          maintainers = [ ];
+        };
+      };
+    });
+
+    devShells = forAllSystems (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [ raylib dejavu_fonts pkg-config ];
+      };
+    });
+
+    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixpkgs-fmt);
+
+    checks = forAllSystems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      default = pkgs.runCommand "cropper-check" { nativeBuildInputs = [ pkgs.gnugrep ]; } ''
+        ${self.packages.${system}.default}/bin/cropper 2>&1 \
+          | grep -q "Usage:" || {
+          echo "FAIL: expected usage message"
+          exit 1
+        }
+        touch $out
       '';
-      installPhase = ''
-        mkdir -p $out/bin
-        cp cropper $out/bin/
-      '';
-    };
+    });
   };
 }
